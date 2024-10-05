39fb5d35b7d66834b56293b107bab7d982e4acd1
Tên của bạn <email.cua.ban@example.com>
2024-10-05 17:29:27 +0700

    update status product

--- admin/index.php
+++ admin/index.php
@@ -87,7 +87,49 @@ $data = executeResult($sql);
 </div>
 
 <!-- ================ Order Details List ================= -->
+<div class="details">
+    <div class="recentOrders">
+        <div class="cardHeader">
+            <h2>Đơn hàng gần đây</h2>
+            <a href="../admin/order/index.php" class="btn">View All</a>
+        </div>
 
+        <table class="table table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">STT</th>
+                    <th scope="col">Tên Khách Hàng</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Giá</th>
+                    <th scope="col">Ngày Tạo</th>
+                    <th scope="col">Trạng Thái</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+                $index = 0;
+                foreach ($data as $item) {
+                    echo '<tr>
+                    <th scope="row">' . (++$index) . '</th>
+                    <td>'  . $item['fullname'] . '</td>
+                    <td>'  . $item['email'] . '</td>
+                    <td>' . number_format($item['total_money']) . ' VNĐ</td>
+                    <td>' . date("d-m-Y", strtotime($item['order_date'])) . '</td>
+                    <td>';
+                    if ($item['status'] == 0) {
+                        echo '<span class="badge badge-warning">Đơn chờ xử lý</span>';
+                    } else if ($item['status'] == 1) {
+                        echo '<span class="badge badge-success">Đang giao hàng</span>';
+                    } else {
+                        echo '<span class="badge badge-danger">Đơn bị hủy</span>';
+                    }
+                    echo '</td>
+                    </tr>';
+                }
+            ?>     
+            </tbody>
+        </table>
+    </div>
     <script type="text/javascript">
 	function changeStatus(id, status) {
 		$.post('form_api.php', {
