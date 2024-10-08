e15bc981650bbe9ebc99c27d97280b3f926e592c
Tên của bạn <email.cua.ban@example.com>
2024-10-08 12:47:49 +0700

    update status products

--- status.php
+++ status.php
@@ -29,15 +29,19 @@ $data = executeResult($sql);
                 foreach ($data as $item) {
                     echo '<tr>
                     <th scope="row">' . (++$index) . '</th>
-                    <td><a href="detail1.php?id=' . $item['id'] . '">' . $item['fullname'] . '</a></td>
-                    <td><a href="detail1.php?id=' . $item['id'] . '">' . $item['email'] . '</a></td>
+                    <td>'  . $item['fullname'] . '</td>
+                    <td>'  . $item['email'] . '</td>
                     <td>' . number_format($item['total_money']) . ' VNĐ</td>
                     <td>' . date("d-m-Y", strtotime($item['order_date'])) . '</td>
                     <td>';
                     if ($item['status'] == 0) {
-                        echo '<span class="badge badge-warning">Đơn chờ xử lý</span>';
+                        echo '<span class="badge badge-warning">Chờ xác nhận</span>';
                     } else if ($item['status'] == 1) {
-                        echo '<span class="badge badge-success">Đang giao hàng</span>';
+                        echo '<span class="badge badge-success">Chờ lấy hàng</span>';
+                    } else if ($item['status'] == 2) {
+                        echo '<span class="badge badge-success">Chờ giao hàng</span>';
+                    } else if ($item['status'] == 3) {
+                        echo '<span class="badge badge-success">Giao thành công</span>';
                     } else {
                         echo '<span class="badge badge-danger">Đơn bị hủy</span>';
                     }
